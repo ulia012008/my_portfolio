eaed8b0564bf9107c8eb151373260a4d1b2b0805
Julia Jaworska <ulia012008@gmail.com>
2025-05-19 13:28:56 +0200

    img

--- src/main.js
+++ src/main.js
@@ -25,12 +25,80 @@ const accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach(item => {
   const button = item.querySelector('.accordion-button');
-  button.addEventListener('click', () => {
-    item.classList.toggle('active');
+  const icon = item.querySelector('.accordion-icon');
 
-    // Закриває інші
+  button.addEventListener('click', () => {
+    const isActive = item.classList.contains('active');
+
+    // Закриває всі
     accordionItems.forEach(i => {
-      if (i !== item) i.classList.remove('active');
+      i.classList.remove('active');
+      i.querySelector('.accordion-icon').textContent = '+';
     });
+
+    // Якщо це неактивне — активувати
+    if (!isActive) {
+      item.classList.add('active');
+      icon.textContent = '–';
+    }
   });
 });
+// slider
+const slides = document.querySelectorAll('.slide');
+const dotsContainer = document.querySelector('.pagination-dots');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let currentSlide = 0;
+
+// Динамічно створюємо крапки
+slides.forEach((_, i) => {
+  const dot = document.createElement('span');
+  dot.classList.add('dot');
+  if (i === 0) dot.classList.add('active');
+  dot.addEventListener('click', () => showSlide(i));
+  dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll('.dot');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+    dots[i].classList.toggle('active', i === index);
+  });
+  currentSlide = index;
+}
+
+nextBtn.addEventListener('click', () => {
+  let next = (currentSlide + 1) % slides.length;
+  showSlide(next);
+});
+
+prevBtn.addEventListener('click', () => {
+  let prev = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(prev);
+});
+
+// form
+emailjs.init('vM-HBhsdHZ-9NnKgQ');
+
+document
+  .getElementById('contact-form')
+  .addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const status = document.getElementById('form-status');
+    status.textContent = 'Sending...';
+
+    emailjs.sendForm('service_68l6vl5', 'template_9a5db6i', this).then(
+      function () {
+        status.textContent = 'Message sent successfully!';
+        document.getElementById('contact-form').reset();
+      },
+      function (error) {
+        console.log(error);
+        status.textContent = 'Something went wrong. Please try again.';
+        status.style.color = 'red';
+      }
+    );
+  });
